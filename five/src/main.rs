28c1fced08e748a8228c8888dd0bcf59548023b1
Christian <christian@avemark.se>
2022-12-11 01:45:10 +0100

    five complete

--- five/src/main.rs
+++ five/src/main.rs
@@ -1,7 +1,42 @@
+use std::{io::BufRead};
+
+use parser::{full_parse, Move};
+
 
 mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    let input_blob = std::io::stdin().lock().lines().map( |line|  line.unwrap()).collect::<Vec<String>>().join("\n");
+
+    match full_parse(&&input_blob[..]) {
+        Ok((_input, (mut stacks, moves))) => {
+            // dbg!(&stacks);
+            // dbg!(&moves);
+            for Move { count, from, to} in moves {
+                let len = stacks[from as usize - 1].len();
+                let drained: Vec<&str> = stacks[from as usize -1]
+                    .drain((len - count as usize)..)
+                    .collect();
+                for crater in drained {
+                    stacks[to as usize - 1].push(crater);
+                };
+            }
+            println!("");
+            for mut stack in stacks {
+                match stack.pop() {
+                    Some(label) => print!("{}", label),
+                    None => print!("[!] "),
+                }
+            }
+        },
+        Err(e) => println!("No good yo: {}", e),
+    };
 }
 
+fn _apply_move_9000(stacks: &mut Vec<Vec<&str>>, crane_operation: Move) {
+    for _ in 0..crane_operation.count {
+        let element = stacks[crane_operation.from as usize - 1].pop().expect("Tried to take from empty stack");
+        stacks[crane_operation.to as usize - 1].push(element);
+    }
+}
+ 
--- five/src/parser.rs
+++ five/src/parser.rs
@@ -1,18 +1,24 @@
-use nom::{character::complete::{self, alpha1}, bytes::complete::tag, sequence::{tuple, preceded, delimited}, multi::{many1, separated_list1}, branch::alt};
+use nom::{character::complete::{self, space1, digit1}, bytes::complete::{tag}, sequence::{tuple, preceded, delimited}, multi::{many1, separated_list1}, branch::alt};
 #[allow(unused_imports)]
 use nom::{IResult};
 
+pub fn full_parse(input: &str) -> IResult<&str, (Vec<Vec<&str>>, Vec<Move>)> {
+  let (input, crate_stacks) = crate_stacks(input)?;
+  let (input, _) = tag(" \n")(input)?;
+  let (input, moves) = crate_moves(input)?;
 
+ 
+  Ok((input, (crate_stacks, moves)))
+}
+
+#[derive(Debug)]
 pub struct Move {
-  count: u32,
-  from: u32,
-  to: u32,
+  pub count: u32,
+  pub from: u32,
+  pub to: u32,
 }
 
 fn crate_move(input: &str) -> IResult<&str, Move> {
-  // let (input, _) = tag("move ")(input)?;
-  // let (input, count) = complete::u32(input)?;
-
   let (input, (count, from, to)) = tuple((
     preceded(tag("move "), complete::u32),
     preceded(tag(" from "), complete::u32),
@@ -22,27 +28,62 @@ fn crate_move(input: &str) -> IResult<&str, Move> {
   return Ok((input, Move { count, from, to}))
 }
 
-
-pub fn crate_moves(i: &str) -> IResult<&str, Vec<Move>> {
-  many1(crate_move)(i)
+fn crate_moves(i: &str) -> IResult<&str, Vec<Move>> {
+  many1(preceded(
+    tag("\n"),
+    crate_move,
+  ))(i)
 }
 
-pub fn crate(i: &str) -> IResult
+fn crate_marker(i: &str) -> IResult<&str, Option<&str>> {
+  let (input, maybecrate) = alt((
+    delimited(
+      tag("["),
+      complete::alpha1,
+      tag("]"),
+    ),
+    tag("   "),
+  ))(i)?;
+
+  let crate_option = match maybecrate {
+    "   " => None,
+    value => Some(value),
+  };
+
+  Ok((input, crate_option))
+}
 
 fn crate_layer(input: &str) -> IResult<&str, Vec<Option<&str>>> {
-  separated_list1(
+  let (input, layer) = separated_list1(
     tag(" "),
-    alt((
-      delimited(
-        tag("["),
-        alpha1,
-        tag("]")
-      ),
-      tag("   "),
-    )),
-  )(input)
+    crate_marker,
+  )(input)?;
+  Ok((input, layer))
 }
 
-pub fn crate_stacks(input: &str) -> IResult<&str, Vec<Vec<&str>>> {
+fn crate_stacks(input: &str) -> IResult<&str, Vec<Vec<&str>>> {
+  let (input, layers) = separated_list1(tag("\n"), crate_layer)(input)?;
+  let (input, _) = tag("\n ")(input)?;
+  let (input, _) = separated_list1(space1, digit1)(input)?;
 
+  let transposed = transpose_crate_stacks(layers);
+  Ok((input, transposed))
+}
+
+fn transpose_crate_stacks(horizontal_stacks: Vec<Vec<Option<&str>>>) -> Vec<Vec<&str>> {
+  let mut vertical_stacks: Vec<Vec<&str>> = vec![];
+  for _ in 0..horizontal_stacks[0].len() {
+    vertical_stacks.push(vec![]);
+  };
+
+  for vec in horizontal_stacks.iter().rev() {
+    for (i, c) in vec.iter().enumerate() {
+      match c {
+        Some(label) => vertical_stacks[i].push(label),
+        None => {},
+      };
+    };
+  };
+
+  vertical_stacks
 }
